2b1b2106cdda671e7536e3732eb10c7e6489c573
David <momachilles@gmail.com>
2024-04-25 16:09:29 +0200

    Add network client.

---
+++ RickAndMorty/Network/Client/RickAndMortyNetworkClient.swift
@@ -0,0 +1,27 @@
+//
+//  RickAndMortyNetworkClient.swift
+//  RickAndMorty
+//
+//  Created by David Alarcon on 25/4/24.
+//
+
+import SwiftUI
+
+protocol RickAndMortyAPI {
+  func characters() async throws -> String
+}
+
+@Observable
+class RickAndMortyNetworkClient {
+  private let networkService: NetworkServiceProtocol
+
+  init(networkService: NetworkServiceProtocol) {
+    self.networkService = networkService
+  }
+}
+
+extension RickAndMortyNetworkClient: RickAndMortyAPI {
+  func characters() async throws -> String {
+    try await networkService.request(from: CharactersEndpoint())
+  }
+}
