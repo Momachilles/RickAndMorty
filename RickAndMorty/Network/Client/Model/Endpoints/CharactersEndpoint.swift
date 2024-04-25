a54341f2edde4f57a8cb77a7abd76f79278b3959
David <momachilles@gmail.com>
2024-04-25 16:08:40 +0200

    All characters endpoint.

---
+++ RickAndMorty/Network/Client/Model/Endpoints/CharactersEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  CharactersEndpoint.swift
+//  RickAndMorty
+//
+//  Created by David Alarcon on 25/4/24.
+//
+
+import Foundation
+
+class CharactersEndpoint: RickAndMortyEndpoint {
+  var path: String { "/character" }
+}
