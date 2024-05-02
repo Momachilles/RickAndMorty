db041ac91d21a4102f6aad0d3824ca5fd734eec0
David <momachilles@gmail.com>
2024-05-02 12:23:06 +0200

    Create episode endpoint.

--- RickAndMorty/Network/Client/Model/Endpoints/EpisodeEndpoint.swift
+++ RickAndMorty/Network/Client/Model/Endpoints/EpisodeEndpoint.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct EpisodeEndpoint: RickAndMortyEndpoint {
+  var urlString: String
+  var path: String { "" }
+  var urlRequest: URLRequest? {
+    guard let url = URL(string: urlString) else { return .none }
+    
+    return URLRequest(url: url)
+  }
+}
