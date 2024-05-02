76f26d56ec70f7f9af5d050d2202ce16007b5c7c
David <momachilles@gmail.com>
2024-05-02 10:30:49 +0200

    Add header in detail.

--- RickAndMorty/Views/Episodes/EpisodeListView.swift
+++ RickAndMorty/Views/Episodes/EpisodeListView.swift
@@ -11,7 +11,38 @@ struct EpisodeListView: View {
   var character: Character
   
   var body: some View {
-    Text("Hello, \(character.name)")
+    List {
+      Section {
+        HStack {
+          AsyncImage(url: URL(string: character.image)) { phase in
+            switch phase {
+            case .empty: ProgressView()
+            case .success(let image):
+              image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            case .failure(_):
+              Image(systemName: "person.crop.circle.badge.exclamationmark")
+            @unknown default:
+              Image(systemName: "person.crop.circle.badge.exclamationmark")
+            }
+          }
+          .frame(width: 100, height: 100)
+          .clipShape(.rect(cornerRadii: .init(topLeading: 10, bottomLeading: 10, bottomTrailing: 10, topTrailing: 10)))
+          Spacer()
+          VStack(alignment: .trailing) {
+            Text(character.name)
+              .font(.title)
+            Text(character.location.name)
+              .font(.headline)
+          }
+        }
+      }
+
+      Section("Episodes") {
+
+      }
+    }
   }
 }
 
