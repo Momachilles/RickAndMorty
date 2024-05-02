f9286ae16b3ee519b45639341faaebc49148dfd4
David <momachilles@gmail.com>
2024-05-02 09:09:52 +0200

    Create character row view.

--- RickAndMorty/Views/Characters/CharacterListview.swift
+++ RickAndMorty/Views/Characters/CharacterListview.swift
@@ -28,36 +28,14 @@ struct CharacterListview: View {
         Text("Error: \(errorMessage)")
           .foregroundColor(.red)
       case .characters(let characters):
-        List {
-          ForEach(characters.results, id: \.id) { character in
-            HStack(spacing: 10) {
-              AsyncImage(url: URL(string: character.image)) { phase in
-                switch phase {
-                case .empty: ProgressView()
-                case .success(let image):
-                  image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                case .failure(let error):
-                  Image(systemName: "person.crop.circle.badge.exclamationmark")
-                @unknown default:
-                  Image(systemName: "person.crop.circle.badge.exclamationmark")
-                }
-              }
-              .frame(width: 50, height: 50)
-              .clipShape(.rect(cornerRadii: .init(topLeading: 10, bottomLeading: 10, bottomTrailing: 10, topTrailing: 10)))
-              VStack(alignment: .leading) {
-                Text(character.name)
-                  .font(.title3)
-                Text(character.location.name)
-                  .font(.caption)
-              }
-              Spacer()
-              Text("\(character.episode.count) ep.")
+        NavigationStack {
+          List {
+            ForEach(characters.results, id: \.id) { character in
+              CharacterRowView(character: character)
             }
           }
+          .navigationTitle("R&M Characters")
         }
-        .navigationTitle("R&M Characters")
       }
     }
     .task {
---
+++ RickAndMorty/Views/Characters/CharacterRowView.swift
@@ -0,0 +1,44 @@
+//
+//  CharacterRowView.swift
+//  RickAndMorty
+//
+//  Created by David Alarcon on 2/5/24.
+//
+
+import SwiftUI
+
+struct CharacterRowView: View {
+  var character: Character
+  
+  var body: some View {
+    HStack(spacing: 10) {
+      AsyncImage(url: URL(string: character.image)) { phase in
+        switch phase {
+        case .empty: ProgressView()
+        case .success(let image):
+          image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        case .failure(let error):
+          Image(systemName: "person.crop.circle.badge.exclamationmark")
+        @unknown default:
+          Image(systemName: "person.crop.circle.badge.exclamationmark")
+        }
+      }
+      .frame(width: 50, height: 50)
+      .clipShape(.rect(cornerRadii: .init(topLeading: 10, bottomLeading: 10, bottomTrailing: 10, topTrailing: 10)))
+      VStack(alignment: .leading) {
+        Text(character.name)
+          .font(.title3)
+        Text(character.location.name)
+          .font(.caption)
+      }
+      Spacer()
+      Text("\(character.episode.count) ep.")
+    }
+  }
+}
+  
+//#Preview {
+//  CharacterRowView()
+//}
