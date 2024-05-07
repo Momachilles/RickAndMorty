c0c03e6027d16f8a1cd08f97a4edf299b6585866
David <momachilles@gmail.com>
2024-05-07 11:22:16 +0200

    Add network service tests.

--- RickAndMortyTests/Network/Service/NetworkServiceTests.swift
+++ RickAndMortyTests/Network/Service/NetworkServiceTests.swift
@@ -6,30 +6,142 @@
 //
 
 import XCTest
+@testable import RickAndMorty
 
 final class NetworkServiceTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  func testNetworkServiceWhenSuccessfulResponse() async throws {
+    /// Given
+    let succesfullMessage = "Test Succesfull"
+    MockURLProtocol.mockResponses = try NetworkServiceTests.successfullResponse(with: succesfullMessage)
+    let sut = NetworkServiceTests.mockNetworkService()
+    
+    /// When
+    let dummyTestModel: DummyTestModel = try await sut.request(from: DummyEndpoint())
+    
+    /// Then
+    XCTAssertEqual(dummyTestModel.message, succesfullMessage)
+  }
+  
+  func testNetworkServiceWhenFailureResponseAsInvalidURL() async {
+    /// Given
+    let sut = NetworkServiceTests.mockNetworkService()
+    
+    /// When / Then
+    await XCTAssertThrowsErrorAsync(
+      try await sut.request(from: DummyInvalidURLEndpoint()) as DummyTestModel,
+      NetworkError.invalidURL
+    )
+  }
+  
+  func testNetworkServiceWhenFailureResponseAsInvalidResponse() async throws {
+    /// Given
+    let sut = NetworkServiceTests.mockNetworkService()
+    let url = try XCTUnwrap(DummyEndpoint().urlRequest?.url)
+    let expectedResponse = try XCTUnwrap(HTTPURLResponse(url: url, statusCode: 401, httpVersion: nil, headerFields: nil))
+    MockURLProtocol.mockResponses = try NetworkServiceTests.failureResponse(with: expectedResponse)
+    
+    /// When / Then
+    await XCTAssertThrowsErrorAsync(
+      try await sut.request(from: DummyEndpoint()) as DummyTestModel,
+      NetworkError.invalidResponse(expectedResponse)
+    )
+  }
+  
+  func testNetworkServiceWhenFailureResponseAsEmptyData() async throws {
+    /// Given
+    let sut = NetworkServiceTests.mockNetworkService()
+    MockURLProtocol.mockResponses = try NetworkServiceTests.failureResponseEmptyData()
+    
+    /// When / Then
+    await XCTAssertThrowsErrorAsync(
+      try await sut.request(from: DummyEndpoint()) as DummyTestModel,
+      NetworkError.emptyData
+    )
+  }
+  
+  func testNetworkServiceWhenWhenFailureResponseAsInvalidJSON() async throws {
+    /// Given
+    let succesfullMessage = "Test Failure"
+    MockURLProtocol.mockResponses = try NetworkServiceTests.failureJSONResponse(with: succesfullMessage)
+    let sut = NetworkServiceTests.mockNetworkService()
+    
+    /// When / Then
+    await XCTAssertThrowsErrorAsync(
+      try await sut.request(from: DummyEndpoint()) as DummyTestModel,
+      NetworkError.invalidJSON(NetworkError.invalidURL)
+    )
+  }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+extension NetworkServiceTests {
+  
+  static func mockNetworkService() -> NetworkServiceProtocol {
+    URLProtocol.registerClass(MockURLProtocol.self)
+    
+    let configuration = URLSessionConfiguration.default
+    configuration.protocolClasses?.insert(MockURLProtocol.self, at: .zero)
+    
+    return NetworkService(configuration: configuration)
+  }
+  
+  static func successfullResponse(with message: String = "Success") throws -> [URL: (Data?, URLResponse?, Error?)] {
+    guard let url = DummyEndpoint().urlRequest?.url else { throw NetworkError.invalidURL }
+    
+    let expectedData = DummyTestModel(message: message).json.data(using: .utf8)
+    let expectedResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+    
+    return [url: (expectedData, expectedResponse, .none)]
+  }
+  
+  static func failureResponse(with response: HTTPURLResponse) throws -> [URL: (Data?, URLResponse?, Error?)] {
+    guard let url = DummyEndpoint().urlRequest?.url else { throw NetworkError.invalidURL }
+    
+    return [url: (.none, response, .none)]
+  }
+  
+  static func failureResponseEmptyData() throws -> [URL: (Data?, URLResponse?, Error?)] {
+    guard let url = DummyEndpoint().urlRequest?.url else { throw NetworkError.invalidURL }
+    
+    let expectedResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+    
+    return [url: (.none, expectedResponse, .none)]
+  }
+  
+  static func failureJSONResponse(with message: String = "Success") throws -> [URL: (Data?, URLResponse?, Error?)] {
+    guard let url = DummyEndpoint().urlRequest?.url else { throw NetworkError.invalidURL }
+    
+    let expectedData = DummyTestModel(message: message).invalidJson.data(using: .utf8)
+    let expectedResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+    
+    return [url: (expectedData, expectedResponse, .none)]
+  }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+extension NetworkError: Equatable {
+  public static func == (lhs: NetworkError, rhs: NetworkError) -> Bool {
+    switch (lhs, rhs) {
+    case
+      (.invalidURL, .invalidURL),
+      (.invalidResponse, .invalidResponse),
+      (.emptyData, .emptyData),
+      (.invalidJSON, .invalidJSON) : return true
+    default: return false
     }
+  }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+extension NetworkError: Comparable {
+  public static func < (lhs: NetworkError, rhs: NetworkError) -> Bool {
+    return lhs.intValue < rhs.intValue
+  }
+  
+  private var intValue: Int {
+    switch self {
+    case .invalidURL: return 0
+    case .invalidResponse: return 1
+    case .emptyData: return 2
+    case .invalidJSON: return 3
     }
-
+  }
 }
